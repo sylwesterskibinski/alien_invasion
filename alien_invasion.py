bd1bef47834860b1cd1eaa1678cab4db0bd23228
Sylwester Skibiński <skibinskisylwester@gmail.com>
2024-05-03 16:50:55 +0200

    added main,ship and settings

---
+++ alien_invasion.py
@@ -0,0 +1,39 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """General class to manage resources and gameplay."""
+    def __init__(self):
+        """Game initialization and creating resources"""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.bg_color = self.settings.bg_color
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+    
+    def run_game(self):
+        """Creating main game loop"""
+        while True:
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+    
+    def _update_screen(self):
+        self.screen.fill(self.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+    
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    """Class responsible for all game settings"""
+    def __init__(self):
+        """Game settings initialization"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
