1ff6ec500101193f90ebdb11e2a0ec8bf0fd5f9e
Sylwester Skibiński <skibinskisylwester@gmail.com>
2024-05-08 20:56:57 +0200

    added options to move left and right

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,6 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
         self.bg_color = self.settings.bg_color
-
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
@@ -20,6 +19,7 @@ class AlienInvasion:
         """Creating main game loop"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -27,7 +27,17 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-    
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    if event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    if event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+
     def _update_screen(self):
         self.screen.fill(self.bg_color)
         self.ship.blitme()
--- ship.py
+++ ship.py
@@ -7,6 +7,14 @@ class Ship:
         self.image = pygame.image.load(r"C:\Users\skibi\Desktop\alien_invasion\images\ship.bmp")
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.moving_right = False
+        self.moving_left = False
+    
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         self.screen.blit(self.image,self.rect)
