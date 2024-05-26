f9b5af1067a83fe53a32f2b3fd047cd2db0c195a
Sylwester Skibiński <skibinskisylwester@gmail.com>
2024-05-26 14:57:16 +0200

    commit

--- ship.py
+++ ship.py
@@ -1,20 +1,30 @@
 import pygame
 
 class Ship:
-    def __init__(self,ai_game):
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings  
         self.screen_rect = ai_game.screen.get_rect()
+
         self.image = pygame.image.load(r"C:\Users\skibi\Desktop\alien_invasion\images\ship.bmp")
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+
         self.moving_right = False
         self.moving_left = False
-    
+
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = int(self.x)
 
     def blitme(self):
-        self.screen.blit(self.image,self.rect)
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
